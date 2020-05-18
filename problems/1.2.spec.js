dd82aa50d4f87ab489c0c226383629c65411dcad
Nathan Evans <nato@Nathans-MacBook-Pro-2.local>
2020-05-17 18:21:34 -0700

    Organize back under outer `describe`

--- problems/1.2.spec.js
+++ problems/1.2.spec.js
@@ -1,30 +1,29 @@
-/**
- * 'How many 10-digit numbers can you make that include all digits 0-9? Numbers starting with 0 do not count.'
- */
-test('There are 3265920 possible permutations using a factorial approach[10! - 9!]', () => {
-	// recursively calculate a factorial
-	function factorial(limit) {
-		if (limit === 0) {
-			return 1
+describe('How many 10-digit numbers can you make that include all digits 0-9? Numbers starting with 0 do not count.', () => {
+	test('There are 3265920 possible permutations using a factorial approach[10! - 9!]', () => {
+		// recursively calculate a factorial
+		function factorial(limit) {
+			if (limit === 0) {
+				return 1
+			}
+			return limit * factorial(limit - 1)
 		}
-		return limit * factorial(limit - 1)
-	}
 
-	expect(factorial(10) - factorial(9)).toEqual(3265920)
-})
+		expect(factorial(10) - factorial(9)).toEqual(3265920)
+	})
 
-test('There are 3265920 possible permutations using a brute force count', () => {
-	function factorial(limit) {
-		// fill an array of arrays for all possible numbers in the 'matrix' then multiply them up
-		return Array(limit)
-			.fill(1)
-			.map(() =>
-				Array(limit)
-					.fill(1)
-					.map((el, index) => index + 1)
-			)
-			.reduce((a, arr) => arr.reduce((b, el) => b * el, 1), 1)
-	}
+	test('There are 3265920 possible permutations using a brute force count', () => {
+		function factorial(limit) {
+			// fill an array of arrays for all possible numbers in the 'matrix' then multiply them up
+			return Array(limit)
+				.fill(1)
+				.map(() =>
+					Array(limit)
+						.fill(1)
+						.map((el, index) => index + 1)
+				)
+				.reduce((a, arr) => arr.reduce((b, el) => b * el, 1), 1)
+		}
 
-	expect(factorial(10) - factorial(9)).toEqual(3265920)
+		expect(factorial(10) - factorial(9)).toEqual(3265920)
+	})
 })
